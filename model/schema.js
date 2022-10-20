e43326a2bb74b5ce65d88c66a9b563a0ed958902
bhanuprakash056 <r19cs056@cit.reva.edu.in>
2022-10-20 17:58:44 +0530

    added addUser api

--- model/schema.js
+++ model/schema.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-    userName:{
+    username:{
         type: 'string',
         required: true,
     },
--- router/router.js
+++ router/router.js
@@ -1,5 +1,16 @@
 import express from 'express';
 const authRouter = express.Router();
+import UserDetails from '../model/schema.js'
+import {addUser} from '../controller/auth.js'
+authRouter.get('/', async (req, res) => {
+    try{
+    const newUser = await UserDetails.find() 
+    res.send(newUser)
+    }
+    catch(err){
+        console.log(err);
+    }
+})
 
-authRouter.get('/', (req, res) => res.send('Hello World!'))
+authRouter.post('/adduser', addUser)
 export default  authRouter;
