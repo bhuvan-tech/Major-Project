2f3fdde9d4451f3e0e8f4db2695f02dd9854d0e0
Bhuvan Y E <r19cs059@cit.reva.edu.in>
2022-10-21 08:39:26 +0530

    restore old code

--- controller/auth.js
+++ controller/auth.js
@@ -4,19 +4,29 @@ import { successResponse } from '../interceptor/success.js';
 import { errorResponse } from '../interceptor/error.js';
 
 const addUser = async (req,res) => {
-    try{ 
-        const userExists = await userDetails.find({ number: req.body.number });
+    let {name,number,password} = req.body;
 
-        if(userExists.length > 0){
-            return errorResponse(res,404,"User already exists");
-        }
-            
-        const user  = await userDetails.create(req.body)
-        return successResponse(res,201,"user added successfully")
-                
+    try{ 
+        userDetails.find({number:number}, async (err,data)=>{
+            var keycount  = Object.keys(data).length;
+
+            if(keycount > 0){
+                return errorResponse(res,406,"User already exists")
+            }
+            else{
+                // variable names for front end are - name, password, number
+                const user = new userDetails()
+                user.username = name;
+                user.number= number;
+                user.password = password;
+                user.save()
+                return successResponse(res,201,"user added successfully")
+            }
+    })
+
     } catch(err){ 
         errorResponse(res,500,err);
     }
 }
 
-export {addUser};
+export {addUser};      
