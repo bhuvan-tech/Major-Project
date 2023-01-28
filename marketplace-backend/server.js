6466a289612c72607df79c8e248d72f33cf75b72
Bhuvan Y E <r19cs059@cit.reva.edu.in>
2023-01-28 19:21:52 +0530

    Frontend and backend of market place

---
+++ marketplace-backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+// test
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status.apply(404).send({ message: 'Product not found' });
+  }
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
