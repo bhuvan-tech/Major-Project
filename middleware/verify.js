f86bb467fe7dcba0219c7d913fd5c6003fa6b681
bhanuprakash056 <r19cs056@cit.reva.edu.in>
2023-03-26 19:17:17 +0530

    made changes to the middleware

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,8 +1,8 @@
 import { errorResponse } from "../interceptor/error.js";
 const valid = (req,res,next) => {
-    const {number} = req.body;
+    const {phone} = req.body;
     if(req.path === "/adduser" || req.path === "/login"){
-        if(number.length !== 10){
+        if(phone.length !== 10){
             return errorResponse(res,400,"invalid number");
         }
     }
