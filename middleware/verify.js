7ffd3fe27f0bf5489ee66380d6207f4f8c9a8430
Bhuvan Y E <r19cs059@cit.reva.edu.in>
2023-04-01 15:15:28 +0530

    removed phone number validation from backend

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,14 +1,14 @@
-import { errorResponse } from "../interceptor/error.js";
-const valid = (req,res,next) => {
-    const {phone} = req.body;
-    if(req.path === "/adduser" || req.path === "/login"){
-        if(phone.length !== 10){
-            return errorResponse(res,400,"invalid number");
-        }
-    }
-    next();
-}
-const password = (req,res,next) => {
+// import { errorResponse } from "../interceptor/error.js";
+// const valid = (req,res,next) => {
+//     const {phone} = req.body;
+//     if(req.path === "/adduser" || req.path === "/login"){
+//         if(phone.length !== 10){
+//             return errorResponse(res,400,"invalid number");
+//         }
+//     }
+//     next();
+// }
+// const password = (req,res,next) => {
     
-}
-export default valid;
+// }
+// export default valid;
--- router/router.js
+++ router/router.js
@@ -1,25 +1,29 @@
 import express from 'express';
 const router = express.Router();
-import {getUsers, addUser, getUserById, updateUser, deleteUser,login} from '../controller/auth.js'
-import valid from '../middleware/verify.js'
-import {validatePassword} from "../middleware/verifypassword.js"
+import {
+  getUsers,
+  addUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+  login,
+} from '../controller/auth.js';
+// import valid from '../middleware/verify.js'
+import { validatePassword } from '../middleware/verifypassword.js';
 
 // 127.0.0.1/api/users
-router
-    .route('/users')
-    .get(getUsers)
-   
+router.route('/users').get(getUsers);
 
-    // 127.0.0.1/api/users/6351e3fb583bae833afb4a13
+// 127.0.0.1/api/users/6351e3fb583bae833afb4a13
 router
-    .route('/users/:id')
-    .get(getUserById)
-    .patch(updateUser)
-    .delete(deleteUser)
+  .route('/users/:id')
+  .get(getUserById)
+  .patch(updateUser)
+  .delete(deleteUser);
 
-router.post('/login',validatePassword,valid, login);
-router.post('/adduser',validatePassword,valid, addUser)
-router.get('/comm',(req,res)=>{
-    res.send("comm");
-})
+router.post('/login', validatePassword, login);
+router.post('/adduser', validatePassword, addUser);
+router.get('/comm', (req, res) => {
+  res.send('comm');
+});
 export default router;
